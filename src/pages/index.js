cacdb6a879ddda26b11466cad60df1446448b372
table66 <64113188+table66@users.noreply.github.com>
2023-07-06 11:20:09 +0800

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,13 +17,13 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          {/* <Link
+          <Link
             className="button button--secondary button--lg"
             to="/my-website/docs/intro">
-            Let's Start !
+            Get Start !
             
-          </Link> */}
-          <SearchBar />
+          </Link>
+          {/* <SearchBar /> */}
         </div>
       </div>
     </header>
