375000b2f4ec7f9ed8c9c92ddf3c5a3c84052917
table66 <64113188+table66@users.noreply.github.com>
2023-07-06 13:33:06 +0800

    搜索框中间

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,14 +16,16 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/my-website/docs/intro">
             Get Start !
             
           </Link>
-          {/* <SearchBar /> */}
+        </div> */}
+        <div className="custom-searchbar-wrapper">
+        <SearchBar />
         </div>
       </div>
     </header>
