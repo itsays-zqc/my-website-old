35e9c7acecc0166310216ed1f0b27ed32ef7f6b4
itsays-zqc <zhouqch@shanghaitech.edu.cn>
2023-07-12 17:18:54 +0800

    getSTARTlink

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/my-website/docs/intro">
+            to="/my-website/docs/faq/">
             Get Start !
             
           </Link>
